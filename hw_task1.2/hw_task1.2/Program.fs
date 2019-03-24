61aca32b5805265efbfce940789a2d8ebed7a59e
Svetuf <ilucha556@gmail.com>
2019-03-24 22:27:16 +0300

    That error again...

--- hw_task1.2/hw_task1.2/Program.fs
+++ hw_task1.2/hw_task1.2/Program.fs
@@ -1,5 +1,9 @@
-﻿let sum a b =
-    a + b
-let mult a b = 
-    a * b
-let d = 5
+﻿let fibonacci n = 
+    let rec loop count firstNum secNum = 
+        match count with
+        | 0 -> secNum
+        | _ -> loop (count - 1) (secNum) (firstNum + secNum)
+    match n with
+    | 1 -> 1
+    | 2 -> 2
+    | x when x > 2  -> loop n 1 1
