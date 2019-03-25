39e14df6456477ce53ab5f6cfb99903123991c1b
Svetuf <ilucha556@gmail.com>
2019-03-25 14:41:05 +0300

    Update Program.fs

--- hw_task1.2/hw_task1.2/Program.fs
+++ hw_task1.2/hw_task1.2/Program.fs
@@ -1,9 +1,14 @@
-﻿let fibonacci n = 
-    let rec loop count firstNum secNum = 
-        match count with
-        | 0 -> secNum
-        | _ -> loop (count - 1) (secNum) (firstNum + secNum)
-    match n with
-    | 1 -> 1
-    | 2 -> 2
-    | x when x > 2  -> loop n 1 1
+﻿
+let rec pow number power =
+    if power = 1 then number
+    else number * pow number (power - 1)
+
+let fillList n m = 
+    let rec loop n list acc =
+        match n with
+        | 0 -> list
+        | _ -> loop (n-1) (list::(acc*2)) acc*2
+
+    loop n (List.init (m+1) (fun i -> pow 2 i))
+
+let a = fillList 5 5
