58ae3a5640f712e3d89848f128906e67509a1c9a
Svetuf <ilucha556@gmail.com>
2019-03-25 22:19:49 +0300

    hw1_task2

--- hw1_task2/hw1_task2.Tests/Program.fs
+++ hw1_task2/hw1_task2.Tests/Program.fs
@@ -1,7 +1,23 @@
-﻿// Дополнительные сведения о F# см. на http://fsharp.org
-// Дополнительную справку см. в проекте "Учебник по F#".
+﻿module tests
 
-[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // возвращение целочисленного кода выхода
+    open NUnit.Framework
+    open FsUnit
+    open logic
+    
+    [<Test>]
+    let ``5 Fibonacci number`` () =
+        fibonacci 5 |> should equal 5
+
+    [<Test>]
+    let ``-1 Fibonacci number`` () =
+        fibonacci 5 |> should equal "The given number is below zero."
+
+    [<Test>]
+    let ``22 Fibonacci number`` () =
+        fibonacci 22 |> should equal 17711
+        
+    [<Test>]
+    let ``0 Fibonacci number`` () =
+        fibonacci 0 |> should equal 0
+        
+    
--- hw1_task2/hw1_task2/Program.fs
+++ hw1_task2/hw1_task2/Program.fs
@@ -1,7 +1,18 @@
-﻿// Дополнительные сведения о F# см. на http://fsharp.org
-// Дополнительную справку см. в проекте "Учебник по F#".
+﻿module logic
 
-[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // возвращение целочисленного кода выхода
+    exception WrongInputError of string
+
+    let fibonacci n = 
+        let rec loop count firstNum secNum = 
+            match count with
+            | 0 -> secNum
+            | _ -> loop (count - 1) (secNum) (firstNum + secNum)
+        match n with
+        | 0 -> 0
+        | 1 -> 1
+        | 2 -> 2
+        | x when x > 2  -> loop (n - 2) 1 1
+        | x when x < 1 -> raise (WrongInputError("The given number is below zero."))
+
+    let index = 18
+    let result = fibonacci index
