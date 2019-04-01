a2cc5cc176dd9b423cf852361d3f3d02be3078ae
Svetuf <ilucha556@gmail.com>
2019-04-01 19:14:06 +0300

    hw2_task2(palindrome)

--- hw2_task2/hw2_task2.Tests/AssemblyInfo.fs
+++ hw2_task2/hw2_task2.Tests/AssemblyInfo.fs
@@ -1,4 +1,4 @@
-﻿namespace hw1_task2.Tests.AssemblyInfo
+﻿namespace hw2_task2.Tests.AssemblyInfo
 
 open System.Reflection
 open System.Runtime.CompilerServices
@@ -7,11 +7,11 @@ open System.Runtime.InteropServices
 // Общие сведения о сборке можно задать с помощью следующего 
 // набора атрибутов. Отредактируйте эти значения атрибутов, чтобы изменить сведения,
 // связанные с этой сборкой.
-[<assembly: AssemblyTitle("hw1_task2.Tests")>]
+[<assembly: AssemblyTitle("hw2_task2.Tests")>]
 [<assembly: AssemblyDescription("")>]
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("HP Inc.")>]
-[<assembly: AssemblyProduct("hw1_task2.Tests")>]
+[<assembly: AssemblyProduct("hw2_task2.Tests")>]
 [<assembly: AssemblyCopyright("Copyright © HP Inc. 2019")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
@@ -22,7 +22,7 @@ open System.Runtime.InteropServices
 [<assembly: ComVisible(false)>]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект видим для COM
-[<assembly: Guid("e32b478e-b273-4ae2-9d49-867af02d9d5f")>]
+[<assembly: Guid("2ce52dcc-8296-4a55-98bf-afc930dfba86")>]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 // 
--- hw2_task2/hw2_task2/AssemblyInfo.fs
+++ hw2_task2/hw2_task2/AssemblyInfo.fs
@@ -1,4 +1,4 @@
-﻿namespace hw1_task2.AssemblyInfo
+﻿namespace hw2_task2.AssemblyInfo
 
 open System.Reflection
 open System.Runtime.CompilerServices
@@ -7,11 +7,11 @@ open System.Runtime.InteropServices
 // Общие сведения о сборке можно задать с помощью следующего 
 // набора атрибутов. Отредактируйте эти значения атрибутов, чтобы изменить сведения,
 // связанные с этой сборкой.
-[<assembly: AssemblyTitle("hw1_task2")>]
+[<assembly: AssemblyTitle("hw2_task2")>]
 [<assembly: AssemblyDescription("")>]
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("HP Inc.")>]
-[<assembly: AssemblyProduct("hw1_task2")>]
+[<assembly: AssemblyProduct("hw2_task2")>]
 [<assembly: AssemblyCopyright("Copyright © HP Inc. 2019")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
@@ -22,7 +22,7 @@ open System.Runtime.InteropServices
 [<assembly: ComVisible(false)>]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект видим для COM
-[<assembly: Guid("2d81425c-3774-424b-a8df-209e164f124d")>]
+[<assembly: Guid("95ded4ee-42e6-4b9c-afa3-7210385fafbe")>]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 // 
