7abfd76241eff56b98ff6e115858979dc609a864
Svetuf <ilucha556@gmail.com>
2019-04-19 18:35:25 +0300

    final Commit

---
+++ hw2_task1/hw2_task1/Program.fs
@@ -0,0 +1,16 @@
+﻿module logic
+
+    let findElement list element =
+        let rec loop count list element =
+            match list with
+            | head::tail -> if head = element
+                            then Some(count)
+                            else loop (count + 1) tail element
+            | [] -> None
+                            
+        loop 1 list element
+
+    let testlist = [1..5]
+
+    let filledList = findElement testlist 6
+
