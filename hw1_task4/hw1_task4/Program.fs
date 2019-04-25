4b873d9f6f653788112511cca9d617c5a5805847
Svetuf <ilucha556@gmail.com>
2019-04-26 00:36:20 +0300

    creating list with powers of 2

---
+++ hw1_task4/hw1_task4/Program.fs
@@ -0,0 +1,8 @@
+﻿module Logic
+
+    let createList n m =
+        let rec loop acc list m =
+            match (List.length list) with
+            | x when x = (m + 1) -> list
+            | _ -> loop (acc / 2) (acc :: list) m
+        loop (pown 2 (n + m)) [] m
