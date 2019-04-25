ab122f8e7bb78fd98ba3e9d7d4720303babab192
Svetuf <ilucha556@gmail.com>
2019-04-26 01:21:23 +0300

    check for palindrome

---
+++ hw2_task2/hw2_task2/Program.fs
@@ -0,0 +1,10 @@
+﻿module logic 
+//open System
+
+    let reverseString (s:string) = new string(Array.rev (s.ToCharArray()))
+
+    let palindrome word =
+        match word with
+        | reversed when reversed = reverseString word -> "it's palindrome"
+        | _ -> "it's not a palindrome"
+
