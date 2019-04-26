8a1fee351ebd4f97f97a7c72c99d7a728ad69298
Svetuf <ilucha556@gmail.com>
2019-04-26 18:07:15 +0300

    palindrome

---
+++ test1_task2/test1_task2.tests/Program.fs
@@ -0,0 +1,9 @@
+﻿module Tests 
+
+    open NUnit.Framework
+    open FsUnit
+    open Logic 
+
+    [<Test>]
+    let ``biggestPalindromeDotOfTwoNumbers should equal 906609`` () =
+        biggestPalindromeDotOfTwoNumbers 1  |> should equal 906609
