80c94bc42abe6b015128b352359c5e48ccf0b532
Svetuf <ilucha556@gmail.com>
2019-05-15 00:15:27 +0300

    Bracket seq

---
+++ hw5_task3/hw5_task1.tests/Program.fs
@@ -0,0 +1,25 @@
+﻿module Tests
+
+    open NUnit.Framework
+    open FsUnit
+    open Logic
+
+    [<Test>]
+    let ``One pair of brackets ()``() =
+        checkBraket "()" |> should equal true
+
+    [<Test>]
+    let ``One pair of brackets []``() =
+        checkBraket "[]" |> should equal true
+
+    [<Test>]
+    let ``One pair of brackets {}``() =
+        checkBraket "{}" |> should equal true
+
+    [<Test>]
+    let ``Here is wrong seq``() =
+        checkBraket "{([Why i dont learn in ITMO?])}(Because i'm cool" |> should equal false
+
+    [<Test>]
+    let ``Empty seq``() =
+        checkBraket "" |> should equal true
---
+++ hw5_task3/hw5_task3/Program.fs
@@ -0,0 +1,20 @@
+﻿module Logic
+
+    /// Main function
+    let checkBraket input =
+        /// Loop is recursive function, counter1 is '(' and ')' brackets,
+        /// counter2 is '[' and ']' brackets, counter3 is '{' and '}' brackets.
+        let rec loop listOfChars counter1 counter2 counter3 =
+            match listOfChars with
+            | [] -> if (counter1 = 0) && (counter2 = 0) && (counter3 = 0) then true
+                    else false
+            | head::tail -> if (counter1 < 0) || (counter2 < 0) || (counter3 < 0) then false
+                            else match head with
+                                 | '(' -> loop tail (counter1 + 1) counter2 counter3
+                                 | ')' -> loop tail (counter1 - 1) counter2 counter3
+                                 | '[' -> loop tail counter1 (counter2 + 1) counter3
+                                 | ']' -> loop tail counter1 (counter2 - 1) counter3
+                                 | '{' -> loop tail counter1 counter2 (counter3 + 1)
+                                 | '}' -> loop tail counter1 counter2 (counter3 - 1)
+                                 | _ -> loop tail counter1 counter2 counter3
+        loop (Seq.toList input) 0 0 0
