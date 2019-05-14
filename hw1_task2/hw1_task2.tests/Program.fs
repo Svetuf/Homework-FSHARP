83b2a009d414d83efc5a9b9428d85282c3835213
Svetuf <ilucha556@gmail.com>
2019-05-14 03:53:49 +0300

    Fibonacci

---
+++ hw1_task2/hw1_task2.tests/Program.fs
@@ -0,0 +1,18 @@
+﻿module Tests
+
+    open NUnit.Framework
+    open FsUnit
+    open Logic
+    
+    [<Test>]
+    let ``5 Fibonacci number`` () =
+        fibonacci 5 |> should equal 5
+
+    [<Test>]
+    let ``22 Fibonacci number`` () =
+        fibonacci 22 |> should equal 17711
+        
+    [<Test>]
+    let ``0 Fibonacci number`` () =
+        fibonacci 0 |> should equal 0
+        
