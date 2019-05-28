00e5adda5191724ddd6ee85c0ffcf66446ae717e
Svetuf <ilucha556@gmail.com>
2019-05-28 17:09:15 +0300

    Parking

---
+++ test1_task3/test1_task3.tests/Program.fs
@@ -0,0 +1,56 @@
+﻿module Tests 
+
+    open NUnit.Framework
+    open FsUnit
+    open Logic 
+    open System.Threading
+
+
+
+    [<Test>]
+    let ``Add two, minus two`` () =
+        let mutable parking_var = parking()
+
+        let mutable firstEntry = entry(parking_var)
+        let mutable secondEntry = entry(parking_var)
+        let mutable thirdEntry = entry(parking_var)
+
+        let someFirstEntry = 
+            async{
+            firstEntry.car_in |> ignore
+            firstEntry.car_in |> ignore
+            }
+
+        let someSecondEntry = 
+            async{
+            secondEntry.car_out |> ignore
+            secondEntry.car_out |> ignore
+            }
+
+        someFirstEntry |> Async.Start
+        someSecondEntry |> Async.Start
+
+        parking_var.free_places_count |> should equal 50
+
+    [<Test>]
+    let ``Add 3 minus 2`` () =
+        let mutable parking_var = parking()
+
+        let mutable firstEntry = entry(parking_var)
+        let mutable secondEntry = entry(parking_var)
+        let someFirstEntry = 
+           async{
+           firstEntry.car_in |> ignore
+           firstEntry.car_in |> ignore
+           firstEntry.car_in |> ignore
+           }
+
+        let someSecondEntry = 
+            async{
+            secondEntry.car_out |> ignore
+            secondEntry.car_out |> ignore
+            }
+        
+        [someFirstEntry; someSecondEntry]  |> Async.Parallel |> Async.RunSynchronously |> ignore
+
+        parking_var.free_places_count |> should equal 49
---
+++ test1_task3/test1_task3/Program.fs
@@ -0,0 +1,38 @@
+﻿module Logic
+
+open System.Threading
+
+    /// A class of parking, its about parking condition.
+    type parking () =
+        /// Maximum places on this parking
+        let mutable max_places = 50
+
+        let mutable free_places = max_places
+
+        /// How much is free now.
+        member this.free_places_count = free_places
+
+        /// What we do if car is go out of parking.
+        member this.car_drive_off =
+            free_places <- Interlocked.Increment(ref this.free_places_count)
+            true
+
+        /// What we do if car is go in parking.
+        member this.car_drive_in =
+            if Interlocked.CompareExchange(ref free_places, 0, 0) > 0 then 
+                free_places <- Interlocked.Decrement(ref this.free_places_count)
+                true
+            else false
+
+    /// A type of entry, there would be 3 pieces.
+    type entry (p : parking) =
+        /// REturn "OK" is car can drive in.
+        member this.car_in = 
+            if p.car_drive_in = true then "OK"
+            else "No place!"
+        
+        member this.car_out = p.car_drive_off
+
+        
+
+
