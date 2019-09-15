3f54eebdca0f33c79a251c3f542d463eafb92f5b
Svetuf <ilucha556@gmail.com>
2019-09-15 17:47:49 +0300

    Point Free style

---
+++ PointFree/PointFree.tests/Program.fs
@@ -0,0 +1,14 @@
+﻿ module Tests
+
+    open NUnit.Framework
+    open FsUnit
+    open Logic
+    open FsCheck.NUnit
+
+    [<Test>]
+    let ``Usual test`` () =
+        func3 () 2 [0; 2; 4; 8; 16] |> should equal [0; 4; 8; 16; 32]
+
+    [<Property>]
+    let ``FsCheck``(x:int, l:list<int>) =        
+        func3 () x l = (List.map (fun y -> y * x) l)
---
+++ PointFree/PointFree/Program.fs
@@ -0,0 +1,6 @@
+﻿module Logic
+
+    let func x l = List.map (fun y -> y * x) l
+    let func1 x = List.map (fun y -> y * x)
+    let func2 x = List.map ((*) x)
+    let func3 ()  = List.map << (*)
