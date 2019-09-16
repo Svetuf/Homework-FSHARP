f8f7a245b3f4fa0fdf7402a0fb689ae1d1c374ca
Svetuf <ilucha556@gmail.com>
2019-09-16 16:36:59 +0300

    First commit

---
+++ hw7_task1/hw7_task1.tests/Program.fs
@@ -0,0 +1,32 @@
+﻿module Tests
+
+    open NUnit.Framework
+    open FsUnit
+    open Logic 
+
+    [<Test>]
+    let ``Test from task``() =
+        let result = RoundFlow 3 {
+            let! a = 2.0 / 12.0
+            let! b = 3.5
+            return a / b
+        }
+        result |> should (equalWithin 0.001) 0.048
+
+    [<Test>]
+    let ``Test namba 2 (accuracy 6)``() =
+        let res = RoundFlow 6 {
+            let! a = 5.0 / 2.0
+            let! b = 0.5
+            return 1.0 / (a + b)
+        }
+        res |> should (equalWithin 0.000001) 0.333333
+
+    [<Test>]
+    let ``Test namba 3(accyracy 9)``() =
+        let res = RoundFlow 9 {
+            let! a = 5.0 / 2.0
+            let! b = 0.5
+            return 1.0 / (a + b)
+        }
+        res |> should (equalWithin 0.000000001) 0.333333333
---
+++ hw7_task1/hw7_task1/Program.fs
@@ -0,0 +1,9 @@
+﻿module Logic
+
+    open System
+
+    type RoundFlow(epsilon: int) =
+        member this.Bind(x: float, f) =
+            f (Math.Round(x, epsilon))
+        member this.Return(x: float) =
+            Math.Round(x, epsilon)
