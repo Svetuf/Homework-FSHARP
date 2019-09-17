2ab77548f0c9320b44a1cd4a674b07aac52c8695
Svetuf <ilucha556@gmail.com>
2019-09-18 01:11:56 +0300

    Corrections

--- hw7_task1/hw7_task1/Program.fs
+++ hw7_task1/hw7_task1/Program.fs
@@ -2,6 +2,7 @@
 
     open System
 
+    // Workflow that makes a calculus with accuracy of epsilon.
     type RoundFlow(epsilon: int) =
         member this.Bind(x: float, f) =
             f (Math.Round(x, epsilon))
