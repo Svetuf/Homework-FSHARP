3013214920a2d5641aebc9e33c9f74d982e7b213
Svetuf <ilucha556@gmail.com>
2019-09-18 02:39:20 +0300

    First commit

---
+++ hw7_task2/hw7_task2.tests/Program.fs
@@ -0,0 +1,25 @@
+﻿module Tests
+
+    open NUnit.Framework
+    open FsUnit
+    open Logic 
+
+    [<Test>]
+    let ``one plus two``() =
+        let result = WorkFlowString() {
+            let! x = "1"
+            let! y = "2"
+            let z = x + y
+            return z
+        }
+        result |> should equal (Some 3)
+     
+    [<Test>]
+    let ``one plus ъ``() =
+        let res = WorkFlowString() {
+            let! x = "1"
+            let! y = "Ъ"
+            let z = x + y
+            return z
+        }
+        res |> should equal None
