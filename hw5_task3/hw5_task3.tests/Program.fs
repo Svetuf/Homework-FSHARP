60b3f2db6548c6cdbec1008a96f4ed9bccaa965f
Svetuf <ilucha556@gmail.com>
2019-09-21 14:37:51 +0300

    Names fixed

--- hw5_task3/hw5_task3.tests/Program.fs
+++ hw5_task3/hw5_task3.tests/Program.fs
@@ -6,57 +6,57 @@
 
     [<Test>]
     let ``Add new write`` () =
-        let mutable book = phoneBook()
+        let mutable book = PhoneBook()
         let mutable res = Map.empty
         let mapToCompare = Map.empty
                               .Add("Person1", "123")
                               .Add("Person2", "456")
-        res <- book.addNote "Person1" "123" res
-        res <- book.addNote "Person2" "456" res
+        res <- book.AddNote "Person1" "123" res
+        res <- book.AddNote "Person2" "456" res
         res |> should equal mapToCompare
 
     [<Test>]
     let ``Find phone by name`` () =
-        let mutable book = phoneBook ()
+        let mutable book = PhoneBook ()
         let mutable res = Map.empty
 
-        res <- book.addNote "Person1" "123" res
-        res <- book.addNote "Person2" "456" res
+        res <- book.AddNote "Person1" "123" res
+        res <- book.AddNote "Person2" "456" res
         
-        book.findPhoneByName "Person1" res |> should equal "123"
+        book.FindPhoneByName "Person1" res |> should equal "123"
         
     [<Test>]
     let ``Find phone by name but wrong`` () =
-        let mutable book = phoneBook ()
+        let mutable book = PhoneBook ()
         let mutable res = Map.empty
 
-        res <- book.addNote "Person1" "123" res
-        res <- book.addNote "Person2" "456" res
+        res <- book.AddNote "Person1" "123" res
+        res <- book.AddNote "Person2" "456" res
 
-        book.findPhoneByName "Person 1" res |> should equal "Nothing"
+        book.FindPhoneByName "Person 1" res |> should equal "Nothing"
 
     [<Test>]
     let ``Find name by phone`` () =
-        let mutable book = phoneBook ()
+        let mutable book = PhoneBook ()
         let mutable res = Map.empty
 
-        res <- book.addNote "Person1" "123" res
-        res <- book.addNote "Person2" "456" res
+        res <- book.AddNote "Person1" "123" res
+        res <- book.AddNote "Person2" "456" res
 
-        book.findNameByPhone "123" res |> should equal "Person1"
+        book.FindNameByPhone "123" res |> should equal "Person1"
 
     [<Test>]
     let ``Serializing`` () =
-        let mutable book = phoneBook ()
+        let mutable book = PhoneBook ()
         
         let mutable toSave = Map.empty
         let mutable res = Map.empty
 
-        toSave <- book.addNote "Person1" "123" toSave
-        toSave <- book.addNote "Person2" "456" toSave
+        toSave <- book.AddNote "Person1" "123" toSave
+        toSave <- book.AddNote "Person2" "456" toSave
 
-        book.serealize toSave
+        book.Serealize toSave
 
-        res <- book.deserealize
+        res <- book.Deserealize
 
         res |> should equal toSave
--- hw5_task3/hw5_task3/Program.fs
+++ hw5_task3/hw5_task3/Program.fs
@@ -5,35 +5,35 @@
     open System
     
     /// A class of book.
-    type phoneBook () = 
+    type PhoneBook () = 
 
         /// Method that add a new note to book.
-        member this.addNote name phone (book: Map<string, string>) = 
+        member this.AddNote name phone (book: Map<string, string>) = 
             if book.ContainsKey name then
                 (book.Remove (book.Item name)).Add (name, phone)
             else book.Add (name, phone)
 
         /// Finding name by given phone, return "Nothing" if not found.
-        member this.findNameByPhone phoneNumber (book: Map<string, string>) =
+        member this.FindNameByPhone phoneNumber (book: Map<string, string>) =
             try
                 Map.findKey (fun key value -> value = phoneNumber) book
             with
                 | :? System.Collections.Generic.KeyNotFoundException as e -> "Nothing"
                 
         /// Finding phone by given name, return "Nothing" if not found.
-        member this.findPhoneByName name (book: Map<string, string>) =
+        member this.FindPhoneByName name (book: Map<string, string>) =
             try
                 Map.find name book
             with
                 | :? System.Collections.Generic.KeyNotFoundException as e -> "Nothing"
 
         /// Printing all the book.
-        member this.printPhoneBook (book: Map<string, string>) =
+        member this.PrintPhoneBook (book: Map<string, string>) =
             for item in book do
                 printfn "%A %A\n" item.Key item.Value
 
         /// Serializing.
-        member this.serealize (book: Map<string, string>) =
+        member this.Serealize (book: Map<string, string>) =
             let writeValue outputStream (x: 'a) =
                 let formatter = new BinaryFormatter()
                 formatter.Serialize(outputStream, box x)
@@ -44,7 +44,7 @@
             printfn "Saved"
         
         /// Deserializing from Phonebook.dat.
-        member this.deserealize =
+        member this.Deserealize =
             let readValue inputStream =
                 let formatter = new BinaryFormatter()
                 let res = formatter.Deserialize(inputStream)
@@ -67,7 +67,7 @@
         printfn "input 6 to save book"
         printfn "input 7 to load book"
 
-        let bookPhone = phoneBook()
+        let bookPhone = PhoneBook()
         let mutable locBook = book
         let input = Console.ReadLine()
 
@@ -76,23 +76,23 @@
         | "2" -> printfn "Input name and phone"
                  let name = Console.ReadLine()
                  let number = Console.ReadLine()
-                 locBook <- bookPhone.addNote name number book
+                 locBook <- bookPhone.AddNote name number book
                  loop locBook
         | "3" -> printfn "input name"
                  let name = Console.ReadLine()
-                 let ans = bookPhone.findPhoneByName name locBook
+                 let ans = bookPhone.FindPhoneByName name locBook
                  Console.WriteLine(ans)
                  loop locBook
         | "4" -> printfn "input phone"
                  let phone = Console.ReadLine()
-                 let ans = bookPhone.findNameByPhone phone locBook
+                 let ans = bookPhone.FindNameByPhone phone locBook
                  Console.WriteLine(ans)
                  loop locBook
-        | "5" -> bookPhone.printPhoneBook locBook
+        | "5" -> bookPhone.PrintPhoneBook locBook
                  loop locBook
-        | "6" -> bookPhone.serealize locBook
+        | "6" -> bookPhone.Serealize locBook
                  loop locBook
-        | "7" -> locBook <- bookPhone.deserealize
+        | "7" -> locBook <- bookPhone.Deserealize
                  loop locBook
         | _ -> printfn "Tis command is not allowed, try again"
                loop locBook
