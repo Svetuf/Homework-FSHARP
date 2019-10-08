9c9b0443c1d15a8b4104e5a2ad6ce22e296e8989
Svetuf <ilucha556@gmail.com>
2019-10-08 15:15:34 +0300

    first commit

---
+++ task3/task3/Program.fs
@@ -0,0 +1,22 @@
+﻿module Logic
+
+    // Thread save stack, supporting parallel pop and push.
+    type ThreadSaveStack<'T>() = 
+        let locker = new obj()
+    
+        let mutable stack : List<'T> = []
+
+        // Thread save push.
+        member this.Push value =
+            lock locker (fun () -> 
+                stack <- value :: stack)
+            
+        // Trying to pop element.
+        member this.TryPop() =
+            lock locker (fun () ->
+                 match stack with
+                 | result :: newStack ->
+                    stack <- newStack
+                    Some result
+                 | [] -> None
+            )  
