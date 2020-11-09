be645df9cf232e6bb364c34bde09415eb041e67c
Svetuf <ilucha556@gmail.com>
2020-11-09 18:08:10 +0300

    Style guide corrections

--- Lambda/Lambda.tests/Tests.fs
+++ Lambda/Lambda.tests/Tests.fs
@@ -8,21 +8,21 @@ module Tests =
         
     [<Test>]
     let ``K I = K*`` () =
-        betaReduce (Body(Lambda('x', Lambda ('y', Var 'x')), Lambda('x', Var 'x'))) |>
+        betaReduce (Application(Lambda('x', Lambda ('y', Var 'x')), Lambda('x', Var 'x'))) |>
         should equal (Lambda('y', Lambda ('x', Var 'x')))
             
     [<Test>]
     let ``I I = I`` () =
-        betaReduce (Body(Lambda('x', Var 'x'), Lambda('x', Var 'x'))) |>
+        betaReduce (Application(Lambda('x', Var 'x'), Lambda('x', Var 'x'))) |>
         should equal (Lambda('x', Var 'x'))
 
     [<Test>]
     let ``K a b``() = 
-       betaReduce (Body(Body(Lambda('x', Lambda('y', Var 'x')), Var 'a'), Var 'b')) |>
-       should equal (Body (Lambda ('y',Var 'a'),Var 'b')) 
+       betaReduce (Application(Application(Lambda('x', Lambda('y', Var 'x')), Var 'a'), Var 'b')) |>
+       should equal (Application (Lambda ('y',Var 'a'),Var 'b')) 
 
     [<Test>]
     let ``((\a.(\b.b b) (\b.b b)) b) ((\c.(c b)) (\a.a))`` () =
-        betaReduce (Body (Body(Body(Lambda('a', Lambda('b', Body(Var('b'), Var('b')))), Lambda('b', Body(Var('b'), Var('b')))), Var('b')), Body(Lambda('c', Body(Var('c'), Var('b'))), Lambda('a', Var('a'))))) |>
-        should equal (Body (Body (Lambda (('b'), Body (Var ('b'), Var ('b'))), Var ('b')), Var ('b')))
+        betaReduce (Application (Application(Application(Lambda('a', Lambda('b', Application(Var('b'), Var('b')))), Lambda('b', Application(Var('b'), Var('b')))), Var('b')), Application(Lambda('c', Application(Var('c'), Var('b'))), Lambda('a', Var('a'))))) |>
+        should equal (Application (Application (Lambda (('b'), Application (Var ('b'), Var ('b'))), Var ('b')), Var ('b')))
         
--- Lambda/Lambda/Program.fs
+++ Lambda/Lambda/Program.fs
@@ -5,50 +5,52 @@ open System
 /// A type representing a lambda term.
 type Term =
     | Var of Char
-    | Body of Term * Term
+    | Application of Term * Term
     | Lambda of Char * Term
 
 /// Finds all free variables in a lambda expression.
-let findFreeVars term=
+let findFreeVars term =
     let rec recGetFV t vars =
         match t with
         | Var var -> var :: vars
-        | Body (S, T) -> (recGetFV S vars) @ (recGetFV T vars)
+        | Application (S, T) -> (recGetFV S vars) @ (recGetFV T vars)
         | Lambda (var, S) -> (recGetFV S vars) |> List.filter (fun x -> x <> var)
     recGetFV term []
 
 /// Finding a new name for a variable.
-let findFreeName S T =
-    (Set.ofSeq ['a' .. 'z'] - Set.ofList ((findFreeVars S) @ (findFreeVars T))).MinimumElement
+let findFreeName s t =
+    (Set.ofSeq ['a' .. 'z'] - Set.ofList ((findFreeVars s) @ (findFreeVars t))).MinimumElement
     
 /// Substitution of the term S into the term T instead of the variable v.
-let rec substitute T v S =
-    match T with
+let rec substitute t v s =
+    let noNameConflict s var t v =
+        (not((findFreeVars s) |> List.contains var) 
+        || (not((findFreeVars t) |> List.contains v)))
+
+    match t with
     | Var x -> 
         if v = x
-        then S
-        else T
-    | Body (T1, T2) ->
-        Body (substitute T1 v S, substitute T2 v S)
-    | Lambda (var, T) -> 
-        match S with
-        | Var (_) when v = var -> T
-        | _ when (not((findFreeVars S) |> List.contains var) 
-                    || (not((findFreeVars T) |> List.contains v))) -> 
-            Lambda (var, substitute T v S)
+        then s
+        else t
+    | Application (t1, t2) ->
+        Application (substitute t1 v s, substitute t2 v s)
+    | Lambda (var, t) -> 
+        match s with
+        | Var (_) when v = var -> t
+        | _ when noNameConflict s var t v ->  Lambda (var, substitute t v s)
         | _ ->
-            Lambda (findFreeName S T, substitute (substitute S var (Var (var))) v T)
+            Lambda (findFreeName s t, substitute (substitute s var (Var (var))) v t)
             
-/// Normal strategy betta reduction algorithm.
+/// Normal strategy beta-reduction algorithm.
 let rec betaReduce term =
     match term with
     | Var v -> Var v
-    | Body (S1, S2) ->
-        match S1 with
-        | Lambda (v, S) -> 
-            betaReduce (substitute S v S2)
+    | Application (s1, s2) ->
+        match s1 with
+        | Lambda (v, s) -> 
+            betaReduce (substitute s v s2)
         | _ -> 
-            Body (betaReduce S1, betaReduce S2)
-    | Lambda (v, T) -> Lambda(v, betaReduce T)
+            Application (betaReduce s1, betaReduce s2)
+    | Lambda (v, t) -> Lambda(v, betaReduce t)
     
         
